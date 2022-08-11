3f0d84272a8399456c81b3f4e01417f43667e59e
jatinqa31 <77570903+jatinqa31@users.noreply.github.com>
2022-08-11 06:02:18 +0530

    2nd commit

--- Dashboard.java
+++ Dashboard.java
@@ -18,12 +18,26 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+//-----------------------------------------------------
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+ 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 import testBase.TestBase;
 
 public class Dashboard extends TestBase{
 
-	public Dashboard(WebDriver driver) {
+ public Dashboard(WebDriver driver) 
+	{
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
@@ -34,7 +48,16 @@ public class Dashboard extends TestBase{
   
   @FindBy(xpath="//input[@id='pyGridActivePage']")
   WebElement input_page;
-   
+  
+  @FindBy(xpath="//table[@id='bodyTbl_right']//tr//th")
+  List<WebElement> Dashboard_header_elms;
+  
+  @FindBy(xpath="//table[@id='bodyTbl_right']//tr")
+  List<WebElement> Dashboard_table_row;
+  
+//  @FindBy(xpath="//table[@id='bodyTbl_right']//tr//td")
+//  List<WebElement> Dashboard_rows;
+  
   @FindBy(xpath="//table[@id='grid-desktop-paginator']/tbody/tr/td[3]/nobr/a")
   WebElement Pagination_table;
   
@@ -44,36 +67,75 @@ public class Dashboard extends TestBase{
   @FindBy(xpath="//a[text()='Next']")
   WebElement Next_page_link;
   
-
+  @FindBy(xpath="//label[@data-test-id='20141007100658002115508']")
+  WebElement record_count;
+  
+  
+  @FindBy(xpath="//a[@id='pui_filter']")
+  WebElement filter_button1;
+  
+  @FindBy(xpath="//data-test-id[@class='20141118104047081580062-DatePicker']")
+  WebElement calander1;
+  
+  @FindBy(xpath="//data-test-id[@class='201411181040470816801140-DatePicker']")
+  WebElement calander2;
+  
   public WebElement Click_inputPage()
   {
 	return input_page; 
   }
 
   
-  public void Click_Maintenance()
+  public void Dashboard_table()
   {
-//	  Actions actions = new Actions(driver);
-//		// Page Up
-//		actions.keyDown(Keys.CONTROL).sendKeys(Keys.UP).build().perform();
-//
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-//		wait.until(ExpectedConditions.elementToBeClickable(Maintenance_cases));
-//		System.out.println("Selected "+Maintenance_cases.isDisplayed());
-//		JavascriptExecutor jse = (JavascriptExecutor)driver;
-//		jse.executeScript("arguments[0].click()", Maintenance_cases);
+try {
+	   		Thread.sleep(3000);
+	   		// Let's get header first
+	   		List<String> allHeaderNames = new ArrayList<String>();
+	   		for (WebElement header : Dashboard_header_elms) {
+	   			String headerName = header.getText();
+	   			allHeaderNames.add(headerName);
+	   		}
+	   		// Each row will be a key value pair. So we will use LinkedHashMap so that order can be retained.All map will be added to a list.
+			List<LinkedHashMap<String, String>> allTableData = new ArrayList<LinkedHashMap<String, String>>();
+			// Get total rows count
+			// Starting from 2 as first row is header. Remember xpath index starts from 1
+			System.out.println("rows count "+ Dashboard_table_row.size());
+			for (int i=2; i<=Dashboard_table_row.size()-1; i++) 
+			{
+				// Getting specific row with each iteration	Locating only cells of specific row. Creating a map to store key-value pair data. It will be created for each iteration of row
+				LinkedHashMap<String, String> eachRowData = new LinkedHashMap<>();
+				// Iterating each cell
+				String specificRowLoc = "//table[@id='bodyTbl_right']//tr[" + i + "]";
+				// Locating only cells of specific row.
+				List<WebElement> allColumnsEle = Dashboard_table_row.get(i).findElements(By.tagName("td"));
+				//driver.findElement(By.xpath(specificRowLoc)).findElements(By.tagName("td"));
+				for (int j=0; j<allColumnsEle.size(); j++) {
+					// Getting cell value
+					String cellValue = allColumnsEle.get(j).getText();
+					// We will put in to map with header name and value with iteration Get jth index value from allHeaderNames and jth cell value of row
+					eachRowData.put(allHeaderNames.get(j), cellValue);
+				}
+				allTableData.add(eachRowData);// After iterating row completely, add in to list.
+	   		}
+			System.out.println(allTableData);	
+}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
   }
+  
 
-  public int pagination_count() throws InterruptedException
+  public void pagination_count() throws InterruptedException
   {
-	  //Thread.sleep(3000);
-//	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-//	wait.until(ExpectedConditions.elementToBeClickable(Next_page_link));
-	   int count=0;
-	   
-	   for (int i=1;i<=5;i++) {
+	  Thread.sleep(3000);
+	  int recordcount=Integer.parseInt(record_count.getText())/5;
+	   for (int i=1;i<=recordcount;i++) {
+//		   waitExplecit(driver,4000,Next_page_link);
 		   Thread.sleep(3000);
 		   dashboard_pages.click();
+		   Dashboard_table();
 	   }
 	   
 	   
@@ -90,12 +152,33 @@ public class Dashboard extends TestBase{
 //	   }
 	   
 
-	   return count;
   }
   
-  public void traverse_page()
+  
+//  public void Click_Maintenance()
+//  {
+//	  	Actions actions = new Actions(driver);
+//		// Page Up
+//		actions.keyDown(Keys.CONTROL).sendKeys(Keys.UP).build().perform();
+//		actions.keyDown(Keys.CONTROL).sendKeys(Keys.UP).build().perform();
+//		WebDriverWait wait = new WebDriverWait(driver, 4000);
+//		wait.until(ExpectedConditions.elementToBeClickable(Maintenance_cases));
+//		System.out.println("Selected "+Maintenance_cases.isDisplayed());
+//		JavascriptExecutor jse = (JavascriptExecutor)driver;
+//		jse.executeScript("arguments[0].click()", Maintenance_cases);
+//  }
+  
+  public void Filter1_click()
   {
+	  filter_button1.click();
+  }
+
+  public void Calander1_click()
+  {
+	  calander1.click();
+  }
+  
+  public void readTableDataInListOfMap(String Key) {
 	  
   }
-    
 }
